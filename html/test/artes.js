1205cba6b01315c197dd0babd36a6cd0f15016a2
MaikolRaw <maikol.raw@gmail.com>
2022-11-05 11:29:52 -0500

    Prueba en github, page

--- html/test/artes.js
+++ html/test/artes.js
@@ -1,4 +1,4 @@
-//arreglo que contiene las respuestas correctas//
+//arreglo que contiene las respuestas correctas
 
 let correctas = [1,1,1,1,1]
 
@@ -14,5 +14,32 @@ function respuesta(num_pregunta, seleccionada){
 //guarda las respuestas elegidas//
 
 opcion_elegida[num_pregunta] = seleccionada.value;
+
+
+id="p" + num_pregunta;
+
+labels=document.getElementById(id).childNodes;
+
+labels[3].style.backgroundColor = "White";
+labels[5].style.backgroundColor = "White";
+labels[7].style.backgroundColor = "White";
+labels[9].style.backgroundColor = "White";
+
+seleccionada.parentNode.style.backgroundColor = "#cec0fc";
+
+
+}
+
+function corregir() {
+
+cantidad_correctas=0;
+for (i=0; i<correctas.length; i++) {
+    if (correctas[i]==opcion_elegida[i]){
+        cantidad_correctas++;
+    }
+
+}
+
+documen.getElementByIdt("resultado").innerHTML = cantidad_correctas;
 
 }
